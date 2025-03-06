f0d71913da1f215d2a0f73d64f2c34cce0c95bc0
I-Zz <21bds022@iiitdwd.ac.in>
2025-03-06 16:12:57 +0530

    added update, update_password, destroy; added :name in users

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -5,4 +5,41 @@ class Users::UsersController < ApplicationController
   def me
     render json: { user: current_user }, status: :ok
   end
+
+  # PATCH /users/update
+  def update
+    if current_user.update(user_params)
+      render json: { message: "User updated successfully", user: current_user }, status: :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /users/update_password
+  def update_password
+    if current_user.update(password_params)
+      render json: { message: "Password updated successfully" }, status: :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/delete
+  def destroy
+    if current_user.destroy
+      render json: { message: "User deleted successfully" }, status: :ok
+    else
+      render json: { errors: "Failed to delete user" }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,18 @@ Rails.application.routes.draw do
 
   namespace :users do
     get "me", to: "users#me"
+    patch "update", to: "users#update"
+    patch "update_password", to: "users#update_password"
+    delete "delete", to: "users#destroy"
   end
+
+  # namespace :users do
+  #   resource :profile, only: [ :show, :update, :destroy ], controller: "users" do
+  #     patch :update_password, on: :collection
+  #   end
+  # end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
